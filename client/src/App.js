ec65a95e0ad705f22cc7e3a89ab4ad63f93aa9b6
Emerson <emerson.bacani@tuitt.com>
2020-04-11 22:31:41 +0800

    fix fetch request

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,16 @@ function App() {
   const [total, setTotal] = useState(0)
   const [jobs, setJobs] = useState([])
   useEffect(() => {
-    fetch("http://localhost:4000")
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Basic ZW1yc25iY25AZ21haWwuY29tOkVtZXJzb25iYWNhbmkxNg==");
+    myHeaders.append("Cookie", "__cfduid=da6a7799b2ef4b057feeff957fd6917111586567317; LDSESSIONID=0il7p8gjmnbfdt9a7nro6mf7o4; YROTSIH=h%3At%3A%7Bm%3AuE%3A%22QMGU9FP7J_U9UUKML_K8%22%3Bm%3AuA%3A%22EYwB1CWZxyiVknDhBy3zAxVBz6%22%3B%7D; SEARCH_SUMMARY=Software+engineer; ___utmvm=###########; ABSSRP=1531; ABSSRPGroup=A; ABHPGroup=A; ABJDGroup=A; HCRSEABOJ=h%3At%3A%7Bm%3AA%3A%22mhplk2%22%3Bm%3Atu0%3A%22%2B%28Zzim_Voww_nlrn%3AmzVnqh3l%29+%2B%28Zzim_Voww_nlrn%3AlyWYyll3%29+%2B%28mYnlm%3At%29+%2B%283lj3oYnxlyn_VY3x_VwhW%3At%29+%2B%28Zzi_mzo3jl_jzkl%3A%28t+A6+tuC+0tu%29%29%22%3B%7D; TBMCookie_15303015096444475054=386827001586588133L+6mDZ+u/2Pb0zJEq9JnPQjjVyo=; __cfruid=c11fc539eb5ccaafd26a863c86f6d7cc0c6a3fef-1586588133");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    fetch("http://localhost:4000", requestOptions)
     .then(res => res.text())
     .then(data => {
       let parser = new DOMParser()
@@ -15,7 +24,7 @@ function App() {
       let result = stringTotal.match(/\d+/g)
       setTotal(Math.max.apply(null, result))
       setJobs(jobPanels)
-      jobPanels.forEach(job => console.log(job.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling))
+      // jobPanels.forEach(job => console.log(job.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling))
     })
   }, [])
   // const showCompanies = [...companies].map((company, i) => <h5 className="card-title">{company.innerText}</h5>)
